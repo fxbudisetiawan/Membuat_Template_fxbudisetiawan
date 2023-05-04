ac721772a8f58aca6eb5dbf6b911137efea8845f
fxbudisetiawan <128137131+fxbudisetiawan@users.noreply.github.com>
2023-05-04 13:11:49 +0700

    Add files via upload

---
+++ v_about.php
@@ -0,0 +1,24 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : fransiskus xaverius</li>
+        <li>Nama Belakang : Budi Setiawan</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="square">
+        <li> Jalan Petamburan V, no.18 Jakarta Pusat</li>
+    </ul>
+ 
+    <h4>Tempat Lahir</h4>
+    <ul type="square">
+        <li>Jakarta/21 April 2001</li>
+    </ul>
+
+    <h4>Hobi</h4>
+    <ul type="square">
+        <li>Bulutangkis</li>
+        <li>Sepak Bola</li>
+    </ul>
+</section>
